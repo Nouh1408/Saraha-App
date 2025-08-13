14da884d96325e1acb69440ce500942527308bf7
Nouh1408 <ahmedinouh@gmail.com>
2025-08-13 16:16:44 +0300

    userModel completed

---
+++ src/DB/model/user.model.js
@@ -0,0 +1,61 @@
+import {Schema, model } from "mongoose";
+const schema = new Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim: true,
+        lowercase:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true
+    },
+    email:{
+        type:String,
+        required:function(){
+            if(this.phoneNumber){
+                return false
+            }
+            return true
+        },
+        trim:true,
+        lowercase:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:String,
+        required:function(){
+            if(this.email){
+                return false
+            }
+            return true
+        },
+        
+    },
+    dob:{
+        type:Date
+    }
+
+},{timestamps:true})
+
+
+schema.virtual("fullname").get(function (){
+    return `${this.firstName} ${this.lastName}`
+})
+
+schema.virtual("fullname").set(function(value){
+    const [firstName,lastName] = value.split(" ")
+    this.firstName = firstName
+    this.lastName = lastName
+})
+schema.virtual("age").get(function(){
+    return new Date().getFullYear() - new Date(this.dob).getFullYear()
+})
+
+export const User = model("User", schema)

0c5f42b91333e6292fd8a0fa2b7c130d5f0a7b84
Nouh1408 <ahmedinouh@gmail.com>
2025-08-23 08:41:39 +0300

    created transporter

--- src/utils/email/index.js
+++ src/utils/email/index.js
@@ -1,4 +1,11 @@
 import nodemailer from 'nodemailer'
 export function sendMail(){
-
+    const transport = nodemailer.createTransport({
+        host:"smtp.gmail.com",
+        port:587,
+        auth:{
+            user:"ahmedinouh@gmail.com",
+            pass:"uiso okzx ryos mbte"
+        }
+    })
 }
